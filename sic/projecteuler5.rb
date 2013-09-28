f62646760426cedb11b5f17b69607fd96d0c2e71
jerome c johnson <jeromecjohnson@Wheezys-Mac-mini.local>
2013-09-28 15:40:32 -0500

    code for ProjectEuler problems 5 and 8

---
+++ sic/projecteuler5.rb
@@ -0,0 +1,31 @@
+# I began with recreating the example in the problem
+# change the range value of 'arr' to 1-10, to verify
+
+# The code seems to be working right, and returns values fine up until
+# the range of 'arr' is increased past 17.  I don't think my computer is
+# powerful enough to deal with the large numbers, the program always crashes!
+# I believe the code would eventually produce the correct answer, if allowed to run
+
+def one_to_twenty(num)
+  arr = (1..20).to_a
+  result = arr.map{|x| num % x}
+  result = result.inject{|memo, x| memo + x}
+  if result == 0
+    true
+  else 
+    false
+  end
+end
+
+def find_it(number)
+  while one_to_twenty(number) == false
+    puts number
+    number +=1
+    break if one_to_twenty(number) == true
+  end
+  puts "The answer is #{number}!"
+end
+
+puts find_it(1)
+
+#122_770_002 last number reached before computer crashed!
